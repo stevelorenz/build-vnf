ab86c7b7fd626ccb01a720aa64a1f440c31c8233
Zuo Xiang <xianglinks@gmail.com>
2018-07-13 11:41:10 +0200

    <test>:  Add a xdp_drop.c for testing eBPF dev env

--- ebpf/setup_ebpf.sh
+++ ebpf/setup_ebpf.sh
@@ -2,13 +2,30 @@
 #
 # setup_dev.sh
 # About: Setup the development environment for eBPF programs
+# Distro: Ubuntu 16.04
+# Ref: Cilium BPF doc
 #
 
 KERNEL_RELEASE="v4.18-rc4"
 
-sudo apt-get install -y make gcc libssl-dev bc libelf-dev libcap-dev \
-    clang gcc-multilib llvm libncurses5-dev git pkg-config libmnl0 bison flex \
-    graphviz
+sudo apt install -y make gcc libssl-dev bc libelf-dev libcap-dev \
+    gcc-multilib libncurses5-dev git pkg-config libmnl0 bison flex \
+    graphviz binutils-dev
+
+# Remove old clang and llvm
+#sudo apt purge -y clang-* llvm-*
+#sudo apt auto-remove
+
+# For kernel releases 4.16+ the BPF selftest has a dependency on LLVM 6.0+
+echo "# LLVM tool chain 6.0" | sudo tee -a /etc/apt/sources.list > /dev/null
+echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main" | sudo tee -a /etc/apt/sources.list > /dev/null
+echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main" | sudo tee -a /etc/apt/sources.list > /dev/null
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+sudo apt update
+sudo apt install -y clang-6.0 lldb-6.0 lld-6.0
+
+sudo ln -s /usr/bin/clang-6.0 /usr/bin/clang
+sudo ln -s /usr/bin/llc-6.0 /usr/bin/llc
 
 if [[ ! -d $HOME/linux ]]; then
     echo "[INFO] Clone Linux source."
@@ -27,3 +44,9 @@ echo "[INFO] Verifier the setup"
 cd "$HOME/linux/tools/testing/selftests/bpf//" || exit
 make
 sudo ./test_verifier
+
+# Compile bpftool
+# bpftool is an essential tool around debugging and introspection of BPF programs and maps
+cd "$HOME/linux/tools/bpf/bpftool/" || exit
+make
+sudo make install
---
+++ ebpf/xdp_drop.c
@@ -0,0 +1,15 @@
+/*
+ * xdp_drop.c
+ * About: A minimal, stand-alone XDP drop program
+ *
+ */
+
+#include <linux/bpf.h>
+
+#ifndef __section
+#define __section(NAME) __attribute__((section(NAME), used))
+#endif
+
+__section("prog") int xdp_drop(struct xdp_md* ctx) { return XDP_DROP; }
+
+char __license[] __section("license") = "GPL";
