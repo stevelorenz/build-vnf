ee0b82a53cd8fa0a17b27a3c58db27c5f28ff3f7
Zuo Xiang <xianglinks@gmail.com>
2018-09-05 13:14:05 +0200

    <feat>: Add a PoC XDP firewall

---
+++ io_visor/vnf/firewall/xdp_firewall.c
@@ -0,0 +1,70 @@
+/*
+ * xdp_firewall.c
+ *
+ * About: eXpress DataPath based firewall
+ *        Compared to common firwall (filter) tools e.g. the iptables
+ *
+ *        XDP based firewall provides more flexibilities and good performance
+ *        at the same time:
+ *        - Protocols in application layer can be also handled. For example,
+ *        HTTP requests can be parsed to drop requests with invalid or banned
+ *        URL before sending to the kernel network stack
+ *
+ * Email: xianglinks@gmail.com
+ *
+ */
+
+#define KBUILD_MODNAME "xdp_firewall"
+
+#ifndef XDP_UTIL_H
+#include "../../../shared_lib/xdp_util.h"
+#endif
+
+#ifndef XDP_ACTION
+#define XDP_ACTION BOUNCE
+#endif
+
+/* Map for TX port */
+BPF_DEVMAP(tx_port, 1);
+
+/* Map for number of received packets */
+BPF_PERCPU_ARRAY(udp_nb_map, long, 1);
+
+/* UDP forwarding functions */
+uint16_t ingress_xdp_redirect(struct xdp_md* ctx)
+{
+        void* data_end = (void*)(long)ctx->data_end;
+        void* data = (void*)(long)ctx->data;
+
+        long* value;
+        uint16_t h_proto;
+        ACTION action = XDP_ACTION;
+        uint64_t nh_off = 0;
+        uint32_t key = 0;
+
+        value = udp_nb_map.lookup(&key);
+        if (value) {
+                *value += 1;
+        }
+
+        nh_off = 0;
+        if (action == BOUNCE) {
+                if (rewrite_mac(data, nh_off, data_end, src_mac, dst_mac)
+                    == OPT_FAIL) {
+                        return XDP_DROP;
+                }
+                return XDP_TX;
+        }
+
+        if (action == REDIRECT) {
+                if (rewrite_mac(data, nh_off, data_end, src_mac, dst_mac)
+                    == OPT_FAIL) {
+                        return XDP_DROP;
+                }
+                return tx_port.redirect_map(0, 0);
+        }
+
+        return XDP_DROP;
+}
+
+uint16_t egress_xdp_tx(struct xdp_md* ctx) { return XDP_TX; }
