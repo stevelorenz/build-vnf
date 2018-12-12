ad65b45c45e546db8a6f1a09c6daa41dfc2b9529
Zuo Xiang <xianglinks@gmail.com>
2018-12-12 16:36:36 +0100

    <perf>: Add tool for page cache hits/miss measurement

--- script/install_bcc.sh
+++ script/install_bcc.sh
@@ -4,8 +4,8 @@
 
 BCC_VER="master"
 
-if [[ $1 = '-b' ]]; then
-    echo "[BCC] Install binaries."
+if [[ $1 = '-p' ]]; then
+    echo "[BCC] Install stable packages. Build only Python2 bindings."
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D4284CDD
     echo "deb https://repo.iovisor.org/apt/xenial xenial main" | sudo tee /etc/apt/sources.list.d/iovisor.list
     sudo apt-get update
@@ -13,7 +13,7 @@ if [[ $1 = '-b' ]]; then
 
 elif [[ $1 = '-s' ]]; then
     # MARK: Used to follow the latest features. e.g. XDP redirecting
-    echo "[BCC] Install from source code."
+    echo "[BCC] Install from source code. Build Python3 bindings"
     echo "# Install requirements."
     # PPA for cmake3
     #sudo apt-get install -y software-properties-common
@@ -21,7 +21,7 @@ elif [[ $1 = '-s' ]]; then
     sudo apt-get update
     sudo apt-get -y install bison build-essential cmake flex git libedit-dev \
         libllvm3.7 llvm-3.7-dev libclang-3.7-dev python zlib1g-dev libelf-dev
-    sudo apt-get install -y linux-headers-"$(uname -r)" python-pyroute2
+    sudo apt-get install -y linux-headers-"$(uname -r)" python-pyroute2 python3-dev python3-pyroute2
     # For Lua support
     sudo apt-get -y install luajit luajit-5.1-dev
     echo "# Compile BCC."
@@ -30,7 +30,7 @@ elif [[ $1 = '-s' ]]; then
     git checkout -b "$BCC_VER"
     mkdir -p ./build
     cd ./build || exit
-    cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+    cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DPYTHON_CMD=python3
     make
     sudo make install
 fi
