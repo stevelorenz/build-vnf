e0d975321268472e74ec49c32d94c1b1eb6ea155
Zuo Xiang <xianglinks@gmail.com>
2019-06-25 22:01:03 +0200

    <fastio_user> Add memory leak check with valgrind

--- fastio_user/tests/run_tests.sh
+++ fastio_user/tests/run_tests.sh
@@ -11,7 +11,11 @@ set -o nounset
 
 TEST_FUNS=(test_ring)
 
+MEM_CHECK=false
 QUITE=false
+
+# Use valgrind to check memory leaks
+MEM_CHECK_CMD="valgrind --leak-check=full"
 
 # Default EAL parameters
 EAL_CORE="-l 0"
@@ -29,19 +33,26 @@ function test_ring() {
     local LOG="./test_ring.log"
 
     echo "- Run ring test..."
-    if [[ $QUITE == true ]]; then
+    if [[ $MEM_CHECK == true ]]; then
+        $MEM_CHECK_CMD ./test_ring.out $EAL_PARAMS
+    elif [[ $QUITE == true ]]; then
         ./test_ring.out $EAL_PARAMS > "$LOG" 2>&1
     else
-        time ./test_ring.out $EAL_PARAMS
+        ./test_ring.out $EAL_PARAMS
     fi
 }
 
 echo "*** Run tests for core functions of fastio_user library"
 echo "--------------------------------------------------------------------------"
 
+# Maybe use getopt is better here
 declare -r opt="${1-"default"}"
-if [[ $opt == "-q" ]]; then
-    echo "* Run in quite mode"
+if [[ $opt == "-c" ]]; then
+    echo "* Run in memory check mode, valgrind is used."
+    MEM_CHECK=true
+
+elif [[ $opt == "-q" ]]; then
+    echo "* Run in quite mode. stdin and stderr are redirected to log files."
     QUITE=true
 fi
 
