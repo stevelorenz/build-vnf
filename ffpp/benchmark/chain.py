64dabafcb480ab70a3002aec4bb20290e1895409
Zuo Xiang <xianglinks@gmail.com>
2019-11-07 14:59:19 +0100

    <ffpp> Add l2fwd test in ffpp benchmark

--- ffpp/benchmark/chain.py
+++ ffpp/benchmark/chain.py
@@ -20,7 +20,7 @@ from mininet.log import info, setLogLevel
 from mininet.node import Controller, OVSSwitch
 
 
-ADD_OF_RULES = True
+ADD_Relay = False
 
 
 def getOFPort(sw, ifce_name):
@@ -30,7 +30,9 @@ def getOFPort(sw, ifce_name):
 
 def testTopo():
 
-    net = Containernet(controller=Controller, link=TCLink, switch=OVSSwitch)
+    net = Containernet(
+        controller=Controller, link=TCLink, switch=OVSSwitch, autoStaticArp=True
+    )
 
     info("*** Adding controller\n")
     net.addController("c0")
@@ -53,7 +55,8 @@ def testTopo():
         dimage="ffpp:latest",
         ip="10.0.0.101/24",
         docker_args={
-            "cpuset_cpus": "0",
+            "cpuset_cpus": "1",
+            "nano_cpus": int(0.6 * 1e9),
             "volumes": {
                 "/sys/bus/pci/drivers": {"bind": "/sys/bus/pci/drivers", "mode": "rw"},
                 "/sys/kernel/mm/hugepages": {
@@ -95,7 +98,8 @@ def testTopo():
         check_output(split(f"ethtool --offload {iface} rx off tx off"))
 
     node_portnum_map = {n: getOFPort(s1, f"s1-{n}") for n in nodes}
-    if ADD_OF_RULES:
+
+    if ADD_Relay:
         info("*** Add OpenFlow rules for traffic redirection.\n")
         check_output(
             split(
@@ -111,19 +115,40 @@ def testTopo():
                 )
             )
         )
-    flow_table = s1.dpctl("dump-flows")
-    print(f"*** Current flow table: \n {flow_table}")
+        check_output(
+            split(
+                'ovs-ofctl add-flow s1 "udp,in_port={},actions=output={}"'.format(
+                    node_portnum_map["server"], node_portnum_map["client"]
+                )
+            )
+        )
+        flow_table = s1.dpctl("dump-flows")
+        print(f"*** Current flow table: \n {flow_table}")
 
-    info("*** Run DPDK helloworld\n")
-    relay.cmd("cd $RTE_SDK/examples/helloworld && make")
-    ret = relay.cmd("cd $RTE_SDK/examples/helloworld/build && ./helloworld")
-    print(f"Output of helloworld app:\n{ret}")
+        info("*** Run DPDK helloworld\n")
+        relay.cmd("cd $RTE_SDK/examples/helloworld && make")
+        ret = relay.cmd("cd $RTE_SDK/examples/helloworld/build && ./helloworld")
+        print(f"Output of helloworld app:\n{ret}")
 
-    info("*** [TODO] Run DPDK l2fwd program on the relay\n")
+        info("*** Run DPDK l2fwd program on the relay\n")
+        relay.cmd("cd $RTE_SDK/examples/l2fwd&& make")
+        run_l2fwd_cmd = " ".join(
+            [
+                "./l2fwd -l 1 -m 256 --vdev=eth_af_packet0,iface=relay-s1",
+                "--no-pci --single-file-segments",
+                "-- -p 1 --no-mac-updating",
+                "> /dev/null &",
+            ]
+        )
+        print(f"The command to run l2fwd: {run_l2fwd_cmd}")
+        ret = relay.cmd(f"cd $RTE_SDK/examples/l2fwd/build && {run_l2fwd_cmd}")
+        print(f"The output of l2fwd app:\n{ret}")
 
-    info("*** Run Sockperf UDP ping-pong test for 10 seconds\n")
+    info("*** Run Sockperf UDP under-load test for 10 seconds\n")
     server.cmd("sockperf server -i %s > /dev/null 2>&1 &" % server.IP())
-    client.cmdPrint("sockperf ping-pong -i %s -t 10" % server.IP())
+    client.cmdPrint(
+        "sockperf under-load -i %s -t 10 --mps 50 --reply-every 1" % server.IP()
+    )
 
     info("*** Enter CLI\n")
     info("Use help command to get CLI usages\n")
@@ -133,10 +158,28 @@ def testTopo():
     net.stop()
 
 
+def usage():
+    print("Usage: sudo python3 chain.py [Option]")
+    print("Option:")
+    print(
+        "\t- (default without any option): Client and server are directly connected, no relay in the middle."
+    )
+    print(
+        "\t- add_relay: Add a relay in the middle running DPDK l2fwd. "
+        "Traffic is redirected via Openflow rules."
+    )
+    sys.exit(0)
+
+
 if __name__ == "__main__":
     setLogLevel("info")
     if len(sys.argv) > 1:
-        if sys.argv[1] == "no_redirect":
-            print("Openflow rules for redirection are not added.")
-            ADD_OF_RULES = False
+        if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+            usage()
+        elif sys.argv[1] == "add_relay":
+            print("Add a relay in the middle running DPDK l2fwd.")
+            ADD_Relay = True
+        else:
+            print("Invalid option!")
+            usage()
     testTopo()
