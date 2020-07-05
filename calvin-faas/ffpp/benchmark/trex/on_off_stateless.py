fd5b0c69ac58a66483304eedc07d82bf65e64a41
Zuo Xiang <xianglinks@gmail.com>
2020-07-05 11:28:40 +0200

    <Fix>: Limit the maximal PPS to 5 MPPS for latency streams.

--- calvin-faas/ffpp/benchmark/trex/on_off_stateless.py
+++ calvin-faas/ffpp/benchmark/trex/on_off_stateless.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """
-About: On/Off traffic of multiple-dependent stateless streams.
+About: On/Off traffic of a deterministic and stateless stream profile.
        For basic latency benchmark of proposed power management mechanisms.
 """
 
 import argparse
+import pprint
 import time
 
 import numpy as np
@@ -18,7 +19,9 @@ from trex.stl.api import (
     STLClient,
     STLError,
     STLFlowLatencyStats,
+    STLFlowStats,
     STLPktBuilder,
+    STLProfile,
     STLStream,
     STLTXCont,
     STLTXSingleBurst,
@@ -32,6 +35,8 @@ IFG_SIZE = 96  # bits
 # Full packet size including Ether, IP and UDP headers
 PACKET_SIZE = 64  # bytes
 
+LATENCY_FLOW_MAX_PPS = 5 * 10 ** 6  # 5 MPPS
+
 
 def init_ports(client):
     all_ports = client.get_all_ports()
@@ -41,66 +46,20 @@ def init_ports(client):
     return (tx_port, rx_port)
 
 
-def add_streams(client, tx_port, stream_profiles):
-    udp_payload_size = PACKET_SIZE - len(Ether()) - len(IP()) - len(UDP())
-    print(f"UDP payload size: {udp_payload_size}")
-    udp_payload = "Z" * udp_payload_size
-    pkt = STLPktBuilder(
-        pkt=Ether()
-        / IP(src="192.168.17.1", dst="192.168.17.2")
-        / UDP(dport=8888, sport=9999, chksum=0)
-        / udp_payload
-    )
-
-    # Add all streams in the stream_profiles
-    streams = list()
-
-    for index, st in enumerate(stream_profiles[:-1]):
-        streams.append(
-            STLStream(
-                name=f"s{index}",
-                isg=st["isg"],
-                packet=pkt,
-                flow_stats=STLFlowLatencyStats(pg_id=index),
-                mode=STLTXSingleBurst(
-                    pps=st["pps"], total_pkts=int(st["pps"] * st["on_time"])
-                ),
-                next=f"s{index+1}",
-            )
-        )
-
-    # Add the last stream without next
-    index = len(stream_profiles) - 1
-    st = stream_profiles[-1]
-    streams.append(
-        STLStream(
-            name=f"s{index}",
-            isg=st["isg"],
-            packet=pkt,
-            flow_stats=STLFlowLatencyStats(pg_id=index),
-            mode=STLTXSingleBurst(
-                pps=st["pps"], total_pkts=int(st["pps"] * st["on_time"])
-            ),
-        )
-    )
-
-    client.add_streams(streams, ports=[tx_port])
-
-
 def start_tx(client, tx_port):
     client.clear_stats()
     client.start(ports=[tx_port])
 
 
-def get_rx_stats(client, tx_port, rx_port, stream_profiles):
+def get_rx_stats(client, tx_port, rx_port, stream_params):
     pgids = client.get_active_pgids()
     print("Currently used pgids: {0}".format(pgids))
     stats = client.get_pgid_stats(pgids["latency"])
 
     # A list of dictionary
-    err_cntrs_results = [dict()] * len(stream_profiles)
-    latency_results = [dict()] * len(stream_profiles)
-    for index, _ in enumerate(stream_profiles):
+    err_cntrs_results = [dict()] * len(stream_params)
+    latency_results = [dict()] * len(stream_params)
+    for index, _ in enumerate(stream_params):
         all_stats = stats["latency"].get(index)
         if not all_stats:
             print(f"No stats available for PG ID {index}!")
@@ -112,14 +71,65 @@ def get_rx_stats(client, tx_port, rx_port, stream_profiles):
     return (err_cntrs_results, latency_results)
 
 
-def generate_stream_profiles(profile, max_bit_rate, on_time, interation):
-    """Generate stream profiles with the link utilization ranging from 0.1 to 1
+def create_streams(stream_params: dict, ip_src: str, ip_dst: str) -> list:
+    """Create a list of STLStream objects."""
+
+    udp_payload_size = PACKET_SIZE - len(Ether()) - len(IP()) - len(UDP())
+    if udp_payload_size < 16:
+        raise RuntimeError("The minimal payload size is 16 bytes.")
+    print(f"UDP payload size: {udp_payload_size}")
+    udp_payload = "Z" * udp_payload_size
+    # UDP checksum is disabled.
+    pkt = STLPktBuilder(
+        pkt=Ether()
+        / IP(src=ip_src, dst=ip_dst)
+        / UDP(dport=8888, sport=9999, chksum=0)
+        / udp_payload
+    )
+
+    streams = list()
+
+    # Ref: https://trex-tgn.cisco.com/trex/doc/trex_stateless.html#_tutorial_per_stream_latency_jitter_packet_errors
+
+    # Latency streams are handled fully by software and do not support at full
+    # line rate like normal streams. Typically, it is sufficient to have a
+    # low-rate latency stream alongside with the real workload stream.
+    # In order to have a latency resolution of 1usec, it is not necessary to
+    # send a latency stream at a speed higher than 1 MPPS. It is suggested not
+    # make the total rate of latency streams higher than 5 MPPS.
+
+    for index, stp in enumerate(stream_params):
+        next_st_name = None
+        if index != len(stream_params) - 1:
+            next_st_name = f"s{index+1}"
+
+        streams.append(
+            STLStream(
+                name=f"s{index}",
+                isg=stp["isg"],
+                packet=pkt,
+                flow_stats=STLFlowLatencyStats(pg_id=index),
+                mode=STLTXSingleBurst(
+                    pps=stp["pps"], total_pkts=int(stp["pps"] * stp["on_time"]),
+                ),
+                next=next_st_name,
+            )
+        )
+
+    return streams
+
+
+def create_stream_params(
+    max_bit_rate: float,
+    on_time: int,
+    init_off_on_ratio: float,
+    iteration: int,
+    test: bool,
+) -> list:
+    """
+    Create stream parameters with the link utilization ranging from 0.1 to 1
     with a step of 0.1.
     """
-    if profile == "deterministic":
-        init_off_on_ratio = 0.5
-    else:
-        raise RuntimeError("Unknown profile!")
 
     pps_list = [
         np.ceil(
@@ -128,25 +138,47 @@ def generate_stream_profiles(profile, max_bit_rate, on_time, interation):
         )
         for utilization in np.arange(0.1, 1.1, 0.1)
     ]
+
+    for pps in pps_list:
+        if pps > LATENCY_FLOW_MAX_PPS:
+            raise RuntimeError(
+                f"The maximal PPS supported by latency streams is {LATENCY_FLOW_MAX_PPS}"
+            )
+
     # In usecs
     isg_list = [
         np.ceil(on_time * init_off_on_ratio * ratio * 10 ** 6)
         for ratio in np.arange(1.0, 0, -0.1)
     ]
-    stream_profiles = [
-        {"pps": pps, "isg": isg, "on_time": on_time}
-        for pps, isg in zip(pps_list, isg_list)
-    ]
 
-    # Duplicate each stream for interation numbers.
-    stream_profiles = [s for s in stream_profiles for _ in range(interation)]
+    if test:
+        stream_params = [
+            {"pps": pps, "isg": isg, "on_time": on_time}
+            for pps, isg in zip(pps_list[:3], isg_list[:3])
+        ]
+    else:
+        stream_params = [
+            {"pps": pps, "isg": isg, "on_time": on_time}
+            for pps, isg in zip(pps_list, isg_list)
+        ]
 
-    return stream_profiles
+    # Duplicate each stream for iteration numbers.
+    stream_params = [s for s in stream_params for _ in range(iteration)]
+
+    return stream_params
 
 
 def main():
 
-    parser = argparse.ArgumentParser(description="ON-OFF stateless traffic generator.")
+    parser = argparse.ArgumentParser(
+        description="On/Off traffic of a deterministic and stateless stream profile."
+    )
+
+    parser.add_argument("ip_src", type=str, help="Source IP address for all streams.")
+    parser.add_argument(
+        "ip_dst", type=str, help="Destination IP address for all streams."
+    )
+
     parser.add_argument(
         "--max_bit_rate",
         type=float,
@@ -155,29 +187,45 @@ def main():
     )
     parser.add_argument("--on_time", type=int, default=2, help="ON time in seconds.")
     parser.add_argument(
-        "--interation",
+        "--init_off_on_ratio",
+        type=float,
+        default=0.5,
+        help="Initial ratio between OFF and ON time.",
+    )
+    parser.add_argument(
+        "--iteration",
         type=int,
         default=1,
-        help="Number of interations for each ON state with different PPS",
+        help="Number of iterations for the ON state of each PPS.",
     )
+    parser.add_argument("--test", action="store_true", help="Just used for debug.")
 
     args = parser.parse_args()
 
-    stream_profiles = generate_stream_profiles(
-        "deterministic", args.max_bit_rate, args.on_time, args.interation
+    stream_params = create_stream_params(
+        args.max_bit_rate,
+        args.on_time,
+        args.init_off_on_ratio,
+        args.iteration,
+        args.test,
     )
-    print("\nTo be used stream profiles:")
-    print(stream_profiles)
+    print("\n--- To be used stream parameters:")
+    pprint.pp(stream_params)
     print()
 
-    RX_DELAY_S = sum([s["on_time"] for s in stream_profiles]) + 3
+    streams = create_streams(stream_params, args.ip_src, args.ip_dst)
+    if args.test:
+        pprint.pp(streams)
+        pprint.pp([s.to_json() for s in streams])
+
+    RX_DELAY_S = sum([s["on_time"] for s in stream_params]) + 3
     RX_DELAY_MS = 1000 * RX_DELAY_S
 
     try:
         client = STLClient()
         client.connect()
         tx_port, rx_port = init_ports(client)
-        add_streams(client, tx_port, stream_profiles)
+        client.add_streams(streams, ports=[tx_port])
         start_ts = time.time()
         start_tx(client, tx_port)
         print(f"The estimated RX delay: {RX_DELAY_S} seconds.")
@@ -186,11 +234,11 @@ def main():
         print(f"Total test duration: {test_dur} seconds")
         # MARK: All latency results are in usec.
         err_cntrs_results, latency_results = get_rx_stats(
-            client, tx_port, rx_port, stream_profiles
+            client, tx_port, rx_port, stream_params
         )
         print("--- The latency results of all streams:")
         print(f"- Number of streams: {len(latency_results)}")
-        for index, _ in enumerate(stream_profiles):
+        for index, _ in enumerate(stream_params):
             print(f"- Stream: {index}")
             print(err_cntrs_results[index])
             print(latency_results[index])
--- calvin-faas/ffpp/benchmark/trex/run_stateless_test.py
+++ calvin-faas/ffpp/benchmark/trex/run_stateless_test.py
@@ -6,19 +6,21 @@
 About: Run simple Trex stateless tests using Python automation API.
 """
 
+import argparse
+import pprint
 import time
 
 import stf_path
 from trex.stl.api import (
-    IP,
-    UDP,
     Ether,
+    IP,
     STLClient,
     STLError,
     STLFlowLatencyStats,
     STLPktBuilder,
     STLStream,
     STLTXSingleBurst,
+    UDP,
 )
 
 PG_ID = 17
@@ -45,8 +47,6 @@ def rx_interation(c, tx_port, rx_port, total_pkts, pkt_len):
         print(
             f"{time.time()}: tx_pkts: {tx_pkts}, tx_pps: {tx_pps}, rx_pkts:{rx_pkts} ,rx_pps: {rx_pps}"
         )
-        print(flow_stats["tx_pkts"])
-        print(flow_stats["rx_pkts"])
     # Block until the TX traffic finishes.
     c.wait_on_traffic(ports=[tx_port])
 
@@ -57,24 +57,43 @@ def rx_interation(c, tx_port, rx_port, total_pkts, pkt_len):
         print("No latency stats available!")
         return False
 
-    print("\n--- Latency stats of the test stream:")
+    print("--- Flow and latency stats of the test stream:")
     drops = lat_stats["err_cntrs"]["dropped"]
     ooo = lat_stats["err_cntrs"]["out_of_order"]
-    print(f"Dropped: {drops}, Out-of-Order: {ooo}")
+    print(f"- Dropped: {drops}, Out-of-Order: {ooo}")
     lat = lat_stats["latency"]
     avg = lat["average"]
     jitter = lat["jitter"]
     hist = lat["histogram"]
     total_max = lat["total_max"]
     print(
-        f"The average latency: {avg} usecs, total max: {total_max} usecs, jitter: {jitter} usecs"
+        f"- The average latency: {avg} usecs, total max: {total_max} usecs, jitter: {jitter} usecs"
     )
-    print(hist)
+    print("The latency histogram:")
+    pprint.pp(hist)
 
     return True
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run basic stateless test with a single latency stream."
+    )
+    parser.add_argument(
+        "--ip_src",
+        type=str,
+        default="192.168.17.1",
+        help="Source IP address for all packets in the stream.",
+    )
+    parser.add_argument(
+        "--ip_dst",
+        type=str,
+        default="192.168.17.2",
+        help="Destination IP address for all packets in the stream.",
+    )
+
+    args = parser.parse_args()
+
     # Create a client for stateless tests.
     clt = STLClient()
     passed = True
@@ -83,7 +102,7 @@ if __name__ == "__main__":
         udp_payload = "A" * 50
         pkt = STLPktBuilder(
             pkt=Ether()
-            / IP(src="192.168.17.1", dst="192.168.17.2")
+            / IP(src=args.ip_src, dst=args.ip_dst)
             / UDP(dport=8888, sport=9999, chksum=0)
             / udp_payload
         )
@@ -102,8 +121,6 @@ if __name__ == "__main__":
         print(f"TX port: {tx_port}, RX port: {rx_port}")
         tx_port_attr = clt.get_port_attr(tx_port)
         rx_port_attr = clt.get_port_attr(rx_port)
-        assert tx_port_attr["src_ipv4"] == "192.168.17.1"
-        assert rx_port_attr["src_ipv4"] == "192.168.18.1"
         clt.reset(ports=all_ports)
         clt.add_streams([st], ports=[tx_port])
         print(f"Inject {TOTAL_PKTS} packets on port {all_ports[0]}")
