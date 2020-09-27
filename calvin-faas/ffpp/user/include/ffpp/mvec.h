900f96a866811b33d166d67cb2b6eea69bec7df6
Zuo Xiang <xianglinks@gmail.com>
2020-09-27 20:43:44 +0200

    <refactor>: Refactor mvec: mbuf vector.

--- calvin-faas/ffpp/user/include/ffpp/mvec.h
+++ calvin-faas/ffpp/user/include/ffpp/mvec.h
@@ -1,15 +1,15 @@
 /*
- * mvec.h
+ * ffpp_mvec.h
  */
 
 #ifndef MVEC_H
 #define MVEC_H
 
+#include <stdint.h>
+
 #include <rte_atomic.h>
 #include <rte_common.h>
 #include <rte_mbuf.h>
-
-#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,49 +18,81 @@ extern "C" {
 /**
  * @file
  *
- * Dynamically resized arrays for pointers of rte_mbufs.
- * This data structure is used to exchange processing units between processes.
+ * (WIP) Dynamically resized arrays for pointers of rte_mbufs.
+ *
+ * One (Main) target use-case of this data structure is used to exchange
+ * processing units between processes.
  *
  */
 
 // The same as the maximal burst size in rte_eth_rx_burst().
-#define MVEC_INIT_CAPACITY 32
+#define MVEC_INIT_CAPACITY 64
 
 /**
  * Macro to interate over all mbufs in a vector.
  */
-#define MVEC_FOREACH_MBUF(i, mbuf, vec)                                        \
-	for (i = 0, mbuf = mvec_at_index((vec), i); i < ffpp_mvec_len((vec));  \
-	     i++, mbuf = mvec_at_index((vec), i))
+#define FFPP_MVEC_FOREACH(vec, i, mbuf)                                        \
+	for (i = 0, mbuf = ffpp_mvec_at_index((vec), i);                       \
+	     i < ffpp_mvec_len((vec));                                         \
+	     i++, mbuf = ffpp_mvec_at_index((vec), i))
 
 /**
- * struct mvec - DPDK Mbuf vector.
+ * struct ffpp_mvec - A vector of rte_mbuf pointers.
  *
  * For performance, this data structure should always be allocated on the stack.
  * To operate headers and payload like a Linux-like socket buffer.
  *
  * Socket buffer reference: http://vger.kernel.org/~davem/skb_data.html
  */
-struct mvec;
+struct ffpp_mvec {
+	uint16_t len; /**< Number of mbufs in the vector */
+	uint16_t capacity; /**< Maximal number of mbufs in the vector */
+	uint16_t socket_id;
+	struct rte_mbuf **head; /**< Head pointer of a rte_mbuf array */
+} __rte_cache_aligned;
 
 /**
- * Create a new mbuf vector.
+ * Initialize the mbuf vector with an array of mbufs.
  *
- * @return: Pointer to the new mbuf vector.
+ * @param vec
+ * @param buf: An array of pointers pointing at mbufs.
+ * @param size: The size of the array.
+ *
  */
-struct mvec *mvec_new(void);
+int ffpp_mvec_init(struct ffpp_mvec *vec, struct rte_mbuf **buf, uint16_t size);
 
 /**
  * Free the given mbuf vector.
- */
-void mvec_free(struct mvec *vec);
-
-/**
- * Free all mbufs in the vector.
  *
  * @param vec
  */
-void mvec_free_mbufs(struct mvec *vec);
+void ffpp_mvec_free(struct ffpp_mvec *vec);
+
+/**
+ * Free all mbufs in the vector (Not the contained mbufs).
+ *
+ * @param vec
+ */
+void ffpp_mvec_free_mbufs(struct ffpp_mvec *vec);
+
+/**
+ * Get the mbuf with the given index.
+ *
+ * @param vec
+ * @param n
+ *
+ * @return 
+ */
+struct rte_mbuf *ffpp_mvec_at_index(struct ffpp_mvec *vec, uint16_t n);
+
+/**
+ * @brief Get the current length of the mbuf vector.
+ *
+ * @param vec
+ *
+ * @return 
+ */
+uint16_t ffpp_mvec_len(struct ffpp_mvec *vec);
 
 /**
  * Free a part of mbufs in the vector.
@@ -68,14 +100,14 @@ void mvec_free_mbufs(struct mvec *vec);
  * @param vec
  * @param offset
  */
-void mvec_free_mbufs_part(struct mvec *vec, uint16_t offset);
+void ffpp_mvec_free_mbufs_part(struct ffpp_mvec *vec, uint16_t offset);
 
 /**
  * Print metadata of a mbuf vector.
  *
  * @param vec
  */
-void print_mvec(struct mvec *vec);
+void ffpp_mvec_print(const struct ffpp_mvec *vec);
 
 /**
  * Compare two mbuf vectors.
@@ -87,7 +119,7 @@ void print_mvec(struct mvec *vec);
  * -1 if vec1 is smaller than vec2 in uint8_t format. +1 if vec1 is bigger.  0 if vec1
  *  and vec2 compare equal.
  */
-int mvec_datacmp(struct mvec *vec1, struct mvec *vec2);
+int ffpp_mvec_datacmp(struct ffpp_mvec *vec1, struct ffpp_mvec *vec2);
 
 /**
  * Like skb_push. Push data to the reserved header room of all mbufs in the
@@ -99,12 +131,12 @@ int mvec_datacmp(struct mvec *vec1, struct mvec *vec2);
  * Raise panic if header room is not enough.
  *
  */
-void mvec_push(struct mvec *vec, uint16_t len);
+void ffpp_mvec_push(struct ffpp_mvec *vec, uint16_t len);
 
-void mvec_push_u8(struct mvec *vec, uint8_t value);
-void mvec_push_u16(struct mvec *vec, uint16_t value);
-void mvec_push_u32(struct mvec *vec, uint32_t value);
-void mvec_push_u64(struct mvec *vec, uint64_t value);
+void ffpp_mvec_push_u8(struct ffpp_mvec *vec, uint8_t value);
+void ffpp_mvec_push_u16(struct ffpp_mvec *vec, uint16_t value);
+void ffpp_mvec_push_u32(struct ffpp_mvec *vec, uint32_t value);
+void ffpp_mvec_push_u64(struct ffpp_mvec *vec, uint64_t value);
 
 /**
  * Like skb_pull - Remove data from the beginning of the data room of all mbufs
@@ -115,12 +147,12 @@ void mvec_push_u64(struct mvec *vec, uint64_t value);
  *
  * Raise panic if data room is not enough.
  */
-void mvec_pull(struct mvec *vec, uint16_t len);
+void ffpp_mvec_pull(struct ffpp_mvec *vec, uint16_t len);
 
-void mvec_pull_u8(struct mvec *vec, uint8_t *values);
-void mvec_pull_u16(struct mvec *vec, uint16_t *values);
-void mvec_pull_u32(struct mvec *vec, uint32_t *values);
-void mvec_pull_u64(struct mvec *vec, uint64_t *values);
+void ffpp_mvec_pull_u8(struct ffpp_mvec *vec, uint8_t *values);
+void ffpp_mvec_pull_u16(struct ffpp_mvec *vec, uint16_t *values);
+void ffpp_mvec_pull_u32(struct ffpp_mvec *vec, uint32_t *values);
+void ffpp_mvec_pull_u64(struct ffpp_mvec *vec, uint64_t *values);
 
 #ifdef __cplusplus
 } /* extern "C" */
--- calvin-faas/ffpp/user/tests/unit/test_mvec.c
+++ calvin-faas/ffpp/user/tests/unit/test_mvec.c
@@ -4,7 +4,10 @@
 
 #include <rte_eal.h>
 
-#include <ffpp/memory.h>
+#include <rte_cycles.h>
+#include <rte_ethdev.h>
+#include <rte_mempool.h>
+#include <ffpp/munf.h>
 #include <ffpp/mvec.h>
 
 #define NUM_MBUFS 5000
@@ -16,15 +19,30 @@ int main(int argc, char *argv[])
 	if (ret < 0)
 		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
 
-	struct rte_mempool *pool;
-	pool = ffpp_init_mempool("test_mvec_pool", NUM_MBUFS, rte_socket_id(),
-				 RTE_MBUF_DEFAULT_BUF_SIZE);
+	struct ffpp_munf_manager_ctx munf_manager_ctx;
+	struct rte_mempool *pool = NULL;
+	ffpp_munf_init_manager(&munf_manager_ctx, "test_manager", pool);
 
-	struct mvec *vec;
-	vec = mvec_new();
-	mvec_free(vec);
+	struct rte_mbuf *rx_buf[64];
+	uint16_t rx_count;
 
-	rte_mempool_free(pool);
+	rx_count = rte_eth_rx_burst(munf_manager_ctx.rx_port_id, 0, rx_buf, 64);
+	assert(rx_count == 64);
+
+	struct ffpp_mvec vec;
+	ffpp_mvec_init(&vec, rx_buf, 64);
+
+	ffpp_mvec_print(&vec);
+	uint16_t i = 0;
+	struct rte_mbuf *m;
+	FFPP_MVEC_FOREACH(&vec, i, m)
+	{
+		assert(rte_pktmbuf_headroom(m) == 128);
+	}
+
+	ffpp_mvec_free(&vec);
+
+	ffpp_munf_cleanup_manager(&munf_manager_ctx);
 
 	rte_eal_cleanup();
 	return 0;
